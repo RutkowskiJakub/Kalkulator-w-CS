f47d3e79b9421e27b10741167ae15b260ddc27d3
Jakubrutkowski <jakubrutkowski95@gmail.com>
2020-05-24 13:44:39 +0200

    Dzialajaca sesja i ciasteczka

---
+++ kalkulator/kalkulator/WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="kalkulator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="X: "></asp:Label>
+            <asp:TextBox ID="TextBoxWartosc1" runat="server" OnTextChanged="TextBoxWartosc1_TextChanged"></asp:TextBox>   
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Y: "></asp:Label>
+            <asp:TextBox ID="TextBoxWartosc2" runat="server" OnTextChanged="TextBoxWartosc2_TextChanged"></asp:TextBox> 
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Operacja: "></asp:Label>
+            <asp:DropDownList ID="DropDownListDzialanie" runat="server" OnSelectedIndexChanged="DropDownListDzialanie_SelectedIndexChanged">
+                <asp:ListItem Value="+">+</asp:ListItem>
+                <asp:ListItem Value="-">-</asp:ListItem>
+                <asp:ListItem Value="*">*</asp:ListItem>
+                <asp:ListItem Value="/">/</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Licz" OnClick="Button1_Click" />
+            <asp:TextBox ID="TextBoxWynik" runat="server"></asp:TextBox>
+        </div>
+    </form>
+   
+</body>
+</html>
---
+++ kalkulator/kalkulator/WebForm1.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace kalkulator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        HttpCookie DzialanieInfo = new HttpCookie("DzialanieInfo");
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["x"] != null && Session["y"] != null && Session["dzialanie"] != null)
+            {
+                TextBoxWartosc1.Text = (Session["x"]).ToString();
+                TextBoxWartosc2.Text = (Session["y"]).ToString();
+                string operacja = (Session["dzialanie"]).ToString();
+                switch (operacja)
+                {
+                    case "+":
+                        DropDownListDzialanie.DataValueField = "+";
+                        DropDownListDzialanie.DataTextField = "+";
+                        DropDownListDzialanie.DataBind();
+                        DropDownListDzialanie.SelectedValue = "+";
+                        break;
+                    case "-":
+                        DropDownListDzialanie.DataValueField = "-";
+                        DropDownListDzialanie.DataTextField = "-";
+                        DropDownListDzialanie.DataBind();
+                        DropDownListDzialanie.SelectedValue = "-";
+                        break;
+                    case "*":
+                        DropDownListDzialanie.DataValueField = "*";
+                        DropDownListDzialanie.DataTextField = "*";
+                        DropDownListDzialanie.DataBind();
+                        DropDownListDzialanie.SelectedValue = "*";
+                        break;
+                    case "/":
+                        DropDownListDzialanie.DataValueField = "/";
+                        DropDownListDzialanie.DataTextField = "/";
+                        DropDownListDzialanie.DataBind();
+                        DropDownListDzialanie.SelectedValue = "/";
+                        break;
+                }
+            }
+            else if (Request.Cookies["DzialanieInfo"] != null)
+            {
+                //    TextBoxWynik.Text = "TEST COOKIES";
+                HttpCookie cookie = Request.Cookies["DzialanieInfo"];
+                TextBoxWartosc1.Text = cookie["a"];
+                TextBoxWartosc2.Text = cookie["b"];
+                //DropDownListDzialanie.SelectedValue = cookie["operacja"];
+                DropDownListDzialanie.DataValueField = cookie["Dzialanie"];
+                DropDownListDzialanie.DataTextField = cookie["Dzialanie"];
+                DropDownListDzialanie.DataBind();
+                DropDownListDzialanie.SelectedValue = cookie["Dzialanie"];
+                TextBoxWynik.Text = cookie["c"];
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(TextBoxWartosc1.Text);
+                double b = double.Parse(TextBoxWartosc2.Text);
+                double c = 0;
+                String operacja = DropDownListDzialanie.SelectedItem.ToString();
+                String dzieleniePrzezZero = "Nie można dzielić przez 0";
+
+
+                switch (operacja)
+                {
+                    case "+":
+                        c = a + b;
+                        break;
+                    case "-":
+                        c = a - b;
+                        break;
+                    case "*":
+                        c = a * b;
+                        break;
+                    case "/":
+                        if (b != 0)
+                        {
+                            c = a / b;
+                        }
+
+                        break;
+                }
+
+                if (operacja == "/" && b == 0)
+                {
+                    TextBoxWynik.Text = dzieleniePrzezZero;
+                }
+                else
+                {
+                    TextBoxWynik.Text = c.ToString();
+                }
+
+                HttpCookie cookie = new HttpCookie("DzialanieInfo");
+                cookie["a"] = a.ToString();
+                cookie["b"] = b.ToString();
+                cookie["Dzialanie"] = operacja;
+                cookie["c"] = c.ToString();
+                DateTime Teraz = DateTime.Now;
+                cookie.Expires = Teraz + new TimeSpan(0, 0, 30, 0);
+                Response.Cookies.Add(cookie);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                //throw;
+            }
+        }
+
+        protected void TextBoxWartosc1_TextChanged(object sender, EventArgs e)
+        {
+            Session["x"] = TextBoxWartosc1.Text;
+        }
+
+        protected void TextBoxWartosc2_TextChanged(object sender, EventArgs e)
+        {
+            Session["y"] = TextBoxWartosc2.Text;
+        }
+
+        protected void DropDownListDzialanie_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Dzialanie"] = DropDownListDzialanie.SelectedItem.ToString();
+        }
+        protected void TextBoxWynik_TextChanged(object sender, EventArgs e)
+        {
+            Session["wynik"] = TextBoxWynik.Text;
+ 
+        }
+
+
+    }
+}
